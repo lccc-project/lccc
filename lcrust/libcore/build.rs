9e218a483955c9eb9cb930f99a7368d44a30a978
Connor Horman <chorman64@gmail.com>
2020-04-18 17:20:13 -0400

    Make libcore work on rustc nightly

---
+++ lcrust/libcore/build.rs
@@ -0,0 +1,4 @@
+
+fn main(){
+    println!("cargo:rustc-env=RUSTC_BOOTSTRAP=1");
+}
--- lcrust/libcore/src/marker.rs
+++ lcrust/libcore/src/marker.rs
@@ -1,3 +1,6 @@
 
 #[lang = "sized"]
 pub trait Sized{}
+
+#[lang = "phantom_data"]
+pub struct PhantomData<T: ?Sized>{}
