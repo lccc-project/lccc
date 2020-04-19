23afca524a62ab94793349b01ffe00b2d0d14b9a
Ray Redondo <rdrpenguin04@gmail.com>
2020-04-18 20:25:56 -0500

    Mip

--- lcrust/lcrust/src/main.rs
+++ lcrust/lcrust/src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+
+static mut progname: &str = "";
+
+fn log() {
+    print!("{}", progname);
+}
+
 fn main() {
-    println!("Test");
+    let mut args = env::args();
+
+    progname = args.next().unwrap().split('/').last().unwrap();
+
+    for arg in args {
+        println!("{:?}", arg);
+    }
 }
