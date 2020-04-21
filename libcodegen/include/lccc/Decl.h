92816e6cf901478bd53ed03fcea9dd46ef3dbd05
Connor Horman <chorman64@gmail.com>
2020-04-20 20:40:39 -0400

    Start unified code generation library and liblcir

---
+++ libcodegen/include/lccc/Decl.h
@@ -0,0 +1,15 @@
+//
+// Created by chorm on 2020-04-20.
+//
+
+#ifndef LCCC_DECL_H
+#define LCCC_DECL_H
+
+#ifdef __cplusplus
+#define LCCC_CDECL extern"C"
+#else
+#define LCCC_CDECL
+#endif
+
+
+#endif //LCCC_DECL_H
