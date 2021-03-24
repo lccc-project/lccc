b9dec1bc2442dd5a743634f08deeba87bffc33f8
Ray Redondo <rdrpenguin04@gmail.com>
2021-03-24 16:16:51 -0500

    Well-programmed stdlib alternative

--- c/src/main.cpp
+++ c/src/main.cpp
@@ -1,7 +1,11 @@
 #include <xlang++/Visit.hpp>
 
+class CVisitor : public lccc::xlang::FileVisitor {
+
+};
+
 extern "C" {
     XLANG_EXPORT lccc::unique_ptr<lccc::xlang::FileVisitor> xlang_plugin_main(lccc::xlang::FileVisitor *parent, lccc::span<lccc::string_view> args) {
-        return lccc::unique_ptr<lccc::xlang::FileVisitor>();
+        return lccc::make_unique<CVisitor>();
     }
 }
