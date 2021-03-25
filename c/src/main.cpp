34d54f9f727565cacd2edde7d4d2e865eadd973a
Ray Redondo <rdrpenguin04@gmail.com>
2021-03-24 20:33:28 -0500

    visitInputFile stub

--- c/src/main.cpp
+++ c/src/main.cpp
@@ -1,8 +1,13 @@
 #include <xlang++/Visit.hpp>
 
 class CVisitor : public lccc::xlang::FileVisitor {
-
+  public:
+    virtual void visitInputFile(FILE *file) override;
 };
+
+void CVisitor::visitInputFile(FILE *file) {
+
+}
 
 extern "C" {
     XLANG_EXPORT lccc::unique_ptr<lccc::xlang::FileVisitor> xlang_plugin_main(lccc::xlang::FileVisitor *parent, lccc::span<lccc::string_view> args) {
