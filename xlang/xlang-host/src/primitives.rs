29a75a89d0e214ecfd461063dc60b8baf8cb08c5
Ray Redondo <rdrpenguin04@gmail.com>
2021-12-14 10:29:26 -0600

    Silence warnings

--- xlang/xlang-host/src/primitives.rs
+++ xlang/xlang-host/src/primitives.rs
@@ -1,28 +1,37 @@
 
 #[cfg(target_pointer_width="16")]
+#[allow(non_camel_case_types)]
 pub type uintptr = u16;
 
 #[cfg(target_pointer_width="32")]
+#[allow(non_camel_case_types)]
 pub type uintptr = u32;
 
 #[cfg(target_pointer_width="64")]
+#[allow(non_camel_case_types)]
 pub type uintptr = u64;
 
 #[cfg(target_pointer_width="16")]
+#[allow(non_camel_case_types)]
 pub type intptr = i16;
 
 #[cfg(target_pointer_width="32")]
+#[allow(non_camel_case_types)]
 pub type intptr = i32;
 
 #[cfg(target_pointer_width="64")]
+#[allow(non_camel_case_types)]
 pub type intptr = i64;
 
 
 #[cfg(target_size_width="16")]
+#[allow(non_camel_case_types)]
 pub type size_t = u16;
 
 #[cfg(target_size_width="32")]
+#[allow(non_camel_case_types)]
 pub type size_t = u32;
 
 #[cfg(target_size_width="64")]
+#[allow(non_camel_case_types)]
 pub type size_t = u64;
