2b7bf62ef969cb61d58c8de40fa9f6dff4beca86
Connor Horman <chorman64@gmail.com>
2021-12-15 21:25:32 -0500

    bitflags sucks

--- xlang/xlang_abi/src/prelude.rs
+++ xlang/xlang_abi/src/prelude.rs
@@ -3,7 +3,6 @@ pub mod v1 {
     pub use crate::collection::HashMap;
     pub use crate::option::{None, Option, Some};
     pub use crate::pair::Pair;
-    pub use crate::result::{Err, Ok, Result};
     pub use crate::string::String;
     pub use crate::traits::{DynBox, DynMut, DynRef};
     pub use crate::vec::Vec;
