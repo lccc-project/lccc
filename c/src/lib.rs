320e9e873126eb10badbd856f1a1c5bf09fd4d3f
Ray Redondo <rdrpenguin04@gmail.com>
2021-12-20 09:30:59 -0600

    Progress on C frontend

--- c/src/lib.rs
+++ c/src/lib.rs
@@ -1,8 +1,39 @@
 #![deny(warnings, clippy::all, clippy::pedantic, clippy::nursery)]
-use xlang::abi::{span::Span, string::StringView};
+use xlang::abi::prelude::v1::*;
+use xlang::abi::result::Result;
+use xlang::abi::string::StringView;
+use xlang::plugin::{Error, XLangFrontend, XLangPlugin};
+use xlang_struct::File;
+
+struct CFrontend {
+    filename: Option<String>,
+}
+
+impl CFrontend {
+    #[must_use]
+    pub const fn new() -> Self {
+        Self { filename: None }
+    }
+}
+
+impl XLangFrontend for CFrontend {
+    fn file_matches(&self, name: StringView) -> bool {
+        name.ends_with(".c") || name.ends_with(".h")
+    }
+
+    fn set_file_path(&mut self, name: StringView) {
+        self.filename = Some(String::from(&*name));
+    }
+}
+
+impl XLangPlugin for CFrontend {
+    fn accept_ir(&mut self, _: &mut File) -> Result<(), Error> {
+        todo!()
+    }
+}
 
 #[allow(clippy::missing_const_for_fn)]
 #[no_mangle]
-pub extern "C" fn xlang_plugin_main(_args: Span<StringView>) /* -> TODO: DynBox<dyn FileVisitor> */
-{
+pub extern "C" fn xlang_plugin_main() -> DynBox<dyn XLangFrontend> {
+    DynBox::unsize_box(Box::new(CFrontend::new()))
 }
