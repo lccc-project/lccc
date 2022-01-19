b07c844709c2398851cd8012481599a59e38fec1
Ray Redondo <rdrpenguin04@gmail.com>
2022-01-19 15:58:14 -0600

    xir: skeleton for xir recompiler

---
+++ xir/src/lib.rs
@@ -0,0 +1,51 @@
+#![deny(warnings, clippy::all, clippy::pedantic, clippy::nursery)]
+use xlang::abi::io::{self, Read};
+use xlang::abi::prelude::v1::*;
+use xlang::abi::result::Result;
+use xlang::abi::string::StringView;
+use xlang::ir;
+use xlang::plugin::{Error, XLangFrontend, XLangPlugin};
+
+struct XirFrontend {
+    filename: Option<String>,
+}
+
+impl XirFrontend {
+    #[must_use]
+    pub const fn new() -> Self {
+        Self {
+            filename: None,
+        }
+    }
+}
+
+impl XLangFrontend for XirFrontend {
+    fn file_matches(&self, name: StringView) -> bool {
+        name.ends_with(".xir")
+    }
+
+    fn set_file_path(&mut self, name: StringView) {
+        self.filename = Some(String::from(&*name));
+    }
+
+    fn read_source(&mut self, _file: DynMut<dyn Read>) -> io::Result<()> {
+        io::Result::Ok(())
+    }
+}
+
+impl XLangPlugin for XirFrontend {
+    #[allow(clippy::too_many_lines)]
+    fn accept_ir(&mut self, _file: &mut ir::File) -> Result<(), Error> {
+        Result::Ok(())
+    }
+
+    fn set_target(&mut self, _targ: xlang::targets::Target) {}
+}
+
+xlang::host::rustcall! {
+    #[allow(clippy::missing_const_for_fn)]
+    #[no_mangle]
+    pub extern "rustcall" fn xlang_frontend_main() -> DynBox<dyn XLangFrontend> {
+        DynBox::unsize_box(Box::new(XirFrontend::new()))
+    }
+}
