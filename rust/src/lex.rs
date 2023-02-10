cdd24a3e218be3dc8ed90dec0b8a30477acc08da
Ray Redondo <raydredondo@gmail.com>
2023-02-09 21:05:55 -0600

    rust: work on lexer

--- rust/src/lex.rs
+++ rust/src/lex.rs
@@ -1,6 +1,9 @@
 use core::iter::Peekable;
 
-use crate::{interning::Symbol, span::Span};
+use crate::{
+    interning::Symbol,
+    span::{HygieneRef, RustEdition, Span, Speekable, Speekerator, HygieneMode, Pos},
+};
 
 use unicode_xid::UnicodeXID;
 
@@ -30,7 +33,13 @@ impl GroupType {
 }
 
 #[derive(Debug)]
-pub enum TokenType {}
+pub enum TokenType {
+    Character,
+    Identifier,
+    Lifetime,
+    Number,
+    String,
+}
 
 #[derive(Debug)]
 pub enum LexemeBody {
@@ -48,7 +57,7 @@ pub struct Lexeme {
 #[derive(Debug)]
 pub enum Error {
     UnexpectedEof,
-    UnrecognizedChar(char),
+    UnrecognizedChar(char, Pos),
 }
 
 #[derive(Debug)]
@@ -56,24 +65,93 @@ pub enum AstFrag {}
 
 pub type Result<T> = core::result::Result<T, Error>;
 
-fn do_lexeme(file: &mut Peekable<impl Iterator<Item = char>>) -> Option<Result<Lexeme>> {
-    match file.next()? {
-        x if x.is_xid_start() || x == '_' => {
-            let mut id = String::from(x);
-            let sym = Symbol::intern_by_val(id);
-            Some(Ok(Lexeme { span: (), body: () }))
+fn do_lexeme(file: &mut Speekable<impl Iterator<Item = char>>) -> Option<Result<Lexeme>> {
+    loop {
+        match file.speek() {
+            Some(&(start, c)) => match c {
+                '"' => {
+                    let mut str = String::from('"');
+                    file.next();
+                    let end = loop {
+                        if let Some((pos, c)) = file.snext() {
+                            str.push(c);
+                            match c {
+                                '"' => break pos,
+                                '\\' => match file.next() {
+                                    Some(c @ ('n' | 'r' | 't' | '\\' | '0' | '"')) => str.push(c),
+                                    _ => todo!(),
+                                }
+                                '\n' => todo!(), // error
+                                x => str.push(x),
+                            }
+                        } else {
+                            return Some(Err(Error::UnexpectedEof));
+                        }
+                    };
+                }
+                x if x.is_xid_start() || x == '_' => {
+                    let mut id = String::from(x);
+                    let mut end = start;
+                    file.next();
+                    while let Some(&(pos, c)) = file.speek() {
+                        if !c.is_xid_continue() {
+                            break;
+                        } else {
+                            id.push(c);
+                            end = pos;
+                            file.next();
+                        }
+                    }
+                    break Some(Ok(Lexeme {
+                        span: Span::new_simple(start, end),
+                        body: LexemeBody::Token {
+                            ty: TokenType::Identifier,
+                            body: id.into(),
+                        },
+                    }));
+                }
+                ' ' => { file.next(); },
+                x => break Some(Err(Error::UnrecognizedChar(x, start))),
+            },
+            None => break None,
         }
-        x => Some(Err(Error::UnrecognizedChar(x))),
     }
 }
 
-pub fn do_lexeme_str(file: &str) -> Result<Lexeme> {
-    do_lexeme(&mut file.chars().peekable()).unwrap_or(Err(Error::UnexpectedEof))
+pub fn do_lexeme_str(token: &str, span: Span) -> Result<Lexeme> {
+    let mut iter = token.chars();
+    let ty = match iter.next() {
+        Some('"') => TokenType::String,
+        Some('0'..='9') => TokenType::Number,
+        Some('\'') => {
+            // Lifetime or char
+            match iter.next() {
+                Some(x) if x.is_xid_start() || x == '_' => match iter.next() {
+                    Some('\'') => TokenType::Character,
+                    _ => TokenType::Lifetime,
+                },
+                _ => TokenType::Character,
+            }
+        }
+        Some(x) if x.is_xid_start() || x.is_xid_continue() => {
+            // TODO: support named identifiers, raw strings
+            TokenType::Identifier
+        }
+        Some(x) => Err(Error::UnrecognizedChar(x, Pos::new(0, 0)))?, // invalid pos b/c we have no idea
+        None => Err(Error::UnexpectedEof)?,
+    };
+    Ok(Lexeme {
+        span,
+        body: LexemeBody::Token {
+            ty,
+            body: token.into(),
+        },
+    })
 }
 
 pub fn lex(file: &mut impl Iterator<Item = char>) -> Result<Vec<Lexeme>> {
     let mut result = Vec::new();
-    let mut file = file.peekable();
+    let mut file = file.speekable();
     while let Some(lexeme) = do_lexeme(&mut file) {
         result.push(lexeme?);
     }
--- rust/src/span.rs
+++ rust/src/span.rs
@@ -1,14 +1,26 @@
-#[derive(Debug)]
+#[derive(Clone, Copy, Debug)]
 pub struct Pos {
-    row: usize,
-    col: usize,
+    pub row: usize,
+    pub col: usize,
+}
+
+impl Pos {
+    pub const fn new(row: usize, col: usize) -> Self {
+        Self { row, col }
+    }
 }
 
 #[derive(Debug)]
 pub struct Span {
-    start: Pos,
-    end: Pos,
-    hygiene: HygieneRef,
+    pub start: Pos,
+    pub end: Pos,
+    pub hygiene: HygieneRef,
+}
+
+impl Span {
+    pub fn new_simple(start: Pos, end: Pos) -> Self {
+        Self { start, end, hygiene: HygieneRef::default() }
+    }
 }
 
 #[derive(Debug, PartialEq, Eq, Hash)]
@@ -16,6 +28,16 @@ pub struct HygieneRef {
     pub hygiene_id: u64,
     pub mode: HygieneMode,
     pub edition: RustEdition,
+}
+
+impl Default for HygieneRef {
+    fn default() -> Self {
+        Self {
+            hygiene_id: 0,
+            mode: HygieneMode::CallSite,
+            edition: RustEdition::Rust2021,
+        }
+    }
 }
 
 #[derive(Debug, PartialEq, Eq, Hash)]
@@ -32,4 +54,68 @@ pub enum RustEdition {
     Rust2018,
     Rust2021,
     Rust2024,
+}
+
+pub trait Speekerator: Iterator<Item = char> + Sized {
+    fn speekable(self) -> Speekable<Self>;
+}
+
+impl<I: Iterator<Item = char>> Speekerator for I {
+    fn speekable(self) -> Speekable<Self> {
+        Speekable::new(self)
+    }
+}
+
+pub struct Speekable<I: Iterator<Item = char>> {
+    inner: I,
+    peeked: Option<(Pos, char)>,
+    pos: Pos,
+}
+
+#[allow(dead_code)]
+impl<I: Iterator<Item = char>> Speekable<I> {
+    const fn new(inner: I) -> Self {
+        Self {
+            inner,
+            peeked: None,
+            pos: Pos::new(1, 1),
+        }
+    }
+
+    fn tick(&mut self) {
+        if self.peeked.is_none() {
+            let c = self.inner.next();
+            if let Some(c) = c {
+                let next_pos = match c {
+                    '\n' => Pos::new(self.pos.row + 1, 1),
+                    _ => Pos::new(self.pos.row, self.pos.col + 1),
+                };
+                self.peeked = Some((self.pos, c));
+                self.pos = next_pos;
+            }
+        }
+    }
+
+    pub fn speek(&mut self) -> Option<&(Pos, char)> {
+        self.tick();
+        self.peeked.as_ref()
+    }
+
+    pub fn peek(&mut self) -> Option<&char> {
+        self.tick();
+        self.peeked.as_ref().map(|(_, x)| x)
+    }
+
+    pub fn snext(&mut self) -> Option<(Pos, char)> {
+        self.tick();
+        self.peeked.take()
+    }
+}
+
+impl<I: Iterator<Item = char>> Iterator for Speekable<I> {
+    type Item = char;
+
+    fn next(&mut self) -> Option<char> {
+        self.peeked.take().map(|(_, x)| x)
+    }
 }
