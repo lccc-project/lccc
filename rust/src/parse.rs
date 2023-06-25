8841274b1e48c937d76201e3ed0eadbab85c80fd
Ray Redondo <raydredondo@gmail.com>
2023-06-25 18:09:37 -0500

    clean up some code, implement Rewinder

--- rust/src/parse.rs
+++ rust/src/parse.rs
@@ -1,4 +1,4 @@
-use core::ops::{BitOr, BitOrAssign};
+use core::ops::{BitOr, BitOrAssign, Deref, DerefMut};
 
 use peekmore::{PeekMore, PeekMoreIterator};
 
@@ -40,22 +40,73 @@ impl BitOrAssign for Error {
 
 pub type Result<T> = core::result::Result<T, Error>;
 
+struct Rewinder<'a, T: Iterator<Item = Lexeme>> {
+    inner: &'a mut PeekMoreIterator<T>,
+    cursor: usize,
+}
+
+impl<T: Iterator<Item = Lexeme>> Rewinder<'_, T> {
+    fn accept(&mut self) {
+        self.cursor = self.inner.cursor(); // We're good; don't rewind when we drop
+    }
+}
+
+impl<'a, T: Iterator<Item = Lexeme>> From<&'a mut PeekMoreIterator<T>> for Rewinder<'a, T> {
+    fn from(inner: &'a mut PeekMoreIterator<T>) -> Self {
+        let cursor = inner.cursor();
+        Self { inner, cursor }
+    }
+}
+
+impl<'a, T: Iterator<Item = Lexeme>> Drop for Rewinder<'a, T> {
+    fn drop(&mut self) {
+        self.inner.move_nth(self.cursor);
+    }
+}
+
+impl<'a, T: Iterator<Item = Lexeme>> Deref for Rewinder<'a, T> {
+    type Target = PeekMoreIterator<T>;
+
+    fn deref(&self) -> &PeekMoreIterator<T> {
+        &self.inner
+    }
+}
+
+impl<'a, T: Iterator<Item = Lexeme>> DerefMut for Rewinder<'a, T> {
+    fn deref_mut(&mut self) -> &mut PeekMoreIterator<T> {
+        &mut self.inner
+    }
+}
+
+trait IntoRewinder<'a> {
+    type Iter: Iterator<Item = Lexeme>;
+    fn into_rewinder(self) -> Rewinder<'a, Self::Iter>;
+}
+
+impl<'a, T: Iterator<Item = Lexeme>> IntoRewinder<'a> for &'a mut PeekMoreIterator<T> {
+    type Iter = T;
+    fn into_rewinder(self) -> Rewinder<'a, T> {
+        Rewinder::from(self)
+    }
+}
+
 pub fn do_lexeme_class(
     tree: &mut PeekMoreIterator<impl Iterator<Item = Lexeme>>,
     expected: LexemeClass,
 ) -> Result<Lexeme> {
+    let mut tree = tree.into_rewinder();
     let lexeme = tree.peek();
     let got = LexemeClass::of(lexeme);
     if got.is(&expected) {
         let result = lexeme.unwrap().clone();
-        tree.advance_cursor();
+        tree.next();
+        tree.accept();
         Ok(result)
     } else {
         let span = lexeme.map_or_else(
             || Span::new_simple(Pos::default(), Pos::default(), ""),
             |x| x.span,
-        );
-        tree.reset_cursor();
+        ); 
         Err(Error {
             expected: vec![expected],
             got,
@@ -68,11 +119,15 @@ pub fn do_lexeme_classes(
     tree: &mut PeekMoreIterator<impl Iterator<Item = Lexeme>>,
     expected: &[LexemeClass],
 ) -> Result<(Lexeme, LexemeClass)> {
+    let mut tree = tree.into_rewinder();
     let mut error = None;
     for &class in expected {
-        let result = do_lexeme_class(tree, class);
+        let result = do_lexeme_class(&mut tree, class);
         match result {
-            Ok(x) => return Ok((x, class)),
+            Ok(x) => {
+                tree.accept();
+                return Ok((x, class))
+            }
             Err(x) => match &mut error {
                 Some(old) => *old |= x,
                 old @ None => *old = Some(x),
@@ -132,13 +187,17 @@ pub fn do_simple_path_segment(
 pub fn do_simple_path(
     tree: &mut PeekMoreIterator<impl Iterator<Item = Lexeme>>,
 ) -> Result<Spanned<SimplePath>> {
-    let from_root = do_lexeme_class(tree, LexemeClass::Punctuation("::".into())).is_ok();
+    let mut tree = tree.into_rewinder();
+    let (from_root, mut span) = match do_lexeme_class(&mut tree, LexemeClass::Punctuation("::".into())) {
+        Ok(x) => (true, x.span),
+        Err(Error { span, .. }) => (false, span),
+    };
     let mut segments = Vec::new();
-    let span = todo!();
     loop {
-        let lexeme = do_simple_path_segment(tree)?;
+        let lexeme = do_simple_path_segment(&mut tree)?;
+        span.end = lexeme.span.end;
         segments.push(lexeme);
-        if do_lexeme_class(tree, LexemeClass::Punctuation("::".into())).is_err() {
+        if do_lexeme_class(&mut tree, LexemeClass::Punctuation("::".into())).is_err() {
             break;
         }
     }
@@ -146,12 +205,14 @@ pub fn do_simple_path(
         from_root,
         segments,
     };
+    tree.accept();
     Ok(Spanned { body: path, span })
 }
 
 pub fn do_attr_body(
     tree: &mut PeekMoreIterator<impl Iterator<Item = Lexeme>>,
 ) -> Result<(Spanned<SimplePath>, Option<AttrInput>)> {
+    // TODO: support attr inputs
     let name = do_simple_path(tree)?;
     Ok((name, None))
 }
@@ -159,22 +220,30 @@ pub fn do_attr_body(
 pub fn do_internal_attr(
     tree: &mut PeekMoreIterator<impl Iterator<Item = Lexeme>>,
 ) -> Result<Spanned<Attr>> {
-    do_lexeme_class(tree, LexemeClass::Punctuation("#".into()))?;
-    do_lexeme_class(tree, LexemeClass::Punctuation("!".into()))?;
-    let (group, span) = do_lexeme_group(tree, Some(GroupType::Brackets))?;
+    let mut tree = tree.into_rewinder();
+    let span_start = do_lexeme_class(&mut tree, LexemeClass::Punctuation("#".into()))?.span;
+    do_lexeme_class(&mut tree, LexemeClass::Punctuation("!".into()))?;
+    let (group, span_end) = do_lexeme_group(&mut tree, Some(GroupType::Brackets))?;
     let (name, input) = do_attr_body(&mut group.body.into_iter().peekmore())?;
+    tree.accept();
     Ok(Spanned {
         body: Attr { name, input },
-        span: todo!(),
+        span: Span::between(span_start, span_end),
     })
 }
 
 pub fn do_external_attr(
     tree: &mut PeekMoreIterator<impl Iterator<Item = Lexeme>>,
 ) -> Result<Spanned<Attr>> {
-    do_lexeme_class(tree, LexemeClass::Punctuation("#".into()))?;
-    let body = do_lexeme_group(tree, Some(GroupType::Brackets))?;
-    todo!()
+    let mut tree = tree.into_rewinder();
+    let span_start = do_lexeme_class(&mut tree, LexemeClass::Punctuation("#".into()))?.span;
+    let (group, span_end) = do_lexeme_group(&mut tree, Some(GroupType::Brackets))?;
+    let (name, input) = do_attr_body(&mut group.body.into_iter().peekmore())?;
+    tree.accept();
+    Ok(Spanned {
+        body: Attr { name, input },
+        span: Span::between(span_start, span_end),
+    })
 }
 
 pub fn do_item(tree: &mut PeekMoreIterator<impl Iterator<Item = Lexeme>>) -> Result<Item> {
@@ -204,8 +273,5 @@ pub fn do_mod(tree: &mut PeekMoreIterator<impl Iterator<Item = Lexeme>>) -> Resu
         }
     }
 
-    Ok(Mod {
-        attrs: Vec::new(),
-        items: Vec::new(),
-    })
+    Ok(Mod { attrs, items })
 }
