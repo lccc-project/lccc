f2b23bb3f19576193ad16163816ae59bf7382749
Ray Redondo <raydredondo@gmail.com>
2023-07-24 16:33:08 -0500

    feat(rust-irgen): continue xir_visitor work

--- rust/src/irgen/xir_visitor.rs
+++ rust/src/irgen/xir_visitor.rs
@@ -1,9 +1,9 @@
 use xlang::ir;
 
-use crate::sema::DefId;
+use crate::{interning::Symbol, sema::DefId};
 
 use super::{
-    visitor::{ModVisitor, TypeDefVisitor, ValueDefVisitor},
+    visitor::{AttrVisitor, FunctionDefVisitor, ModVisitor, TypeDefVisitor, ValueDefVisitor},
     NameMap,
 };
 
@@ -30,6 +30,41 @@ impl<'a> ModVisitor for XirModVisitor<'a> {
     }
 
     fn visit_value(&mut self) -> Option<Box<dyn ValueDefVisitor + '_>> {
+        Some(Box::new(XirValueDefVisitor::new(self.names, self.file)))
+    }
+}
+
+pub struct XirValueDefVisitor<'a> {
+    names: &'a NameMap,
+    file: &'a mut ir::File,
+    name: Option<Symbol>,
+}
+
+impl<'a> XirValueDefVisitor<'a> {
+    pub fn new(names: &'a NameMap, file: &'a mut ir::File) -> Self {
+        Self {
+            names,
+            file,
+            name: None,
+        }
+    }
+}
+
+impl<'a> ValueDefVisitor for XirValueDefVisitor<'a> {
+    fn visit_defid(&mut self, defid: DefId) {
+        self.name = Some(self.names[&defid]);
+    }
+
+    fn visit_name(&mut self, _: &[Symbol]) {
+        // already covered by pre-mangling
+    }
+
+    fn visit_attr(&mut self) -> Option<Box<dyn AttrVisitor + '_>> {
+        // don't care yet
+        None
+    }
+
+    fn visit_function(&mut self) -> Option<Box<dyn FunctionDefVisitor + '_>> {
         todo!()
     }
 }
