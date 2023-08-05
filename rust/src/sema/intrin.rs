806572b094f4f88a4a80f834bfd2b8e350c09d40
Connor Horman <chorman64@gmail.com>
2023-08-04 22:41:50 -0400

    feat(rust-intrin): Add {size,align}_of{,_val}

--- rust/src/sema/intrin.rs
+++ rust/src/sema/intrin.rs
@@ -210,11 +210,19 @@ def_intrinsics! {
     intrin discriminant<type, type>(&Var<0>) -> Var<1>;
     unsafe intrin transmute<type, type>(Var<0>) -> Var<1>;
     intrin black_box<type>(Var<0>) -> Var<1>;
+
     unsafe intrin construct_in_place<type, type, type>(*mut Var<0>, Var<1>, Var<2>) -> ();
+
     unsafe intrin __builtin_read<type>(*const Var<0>) -> Var<0>;
     unsafe intrin __builtin_read_freeze<type>(*const Var<0>) -> Var<0>;
     unsafe intrin __builtin_read_volatile<type>(*const Var<0>) -> Var<0>;
     unsafe intrin __builtin_write<type>(*mut Var<0>,Var<0>) -> ();
     unsafe intrin __builtin_write_volatile<type>(*mut Var<0>, Var<0>) -> ();
 
+
+    intrin __builtin_size_of<type>() -> usize;
+    intrin __builtin_align_of<type>() -> usize;
+
+    intrin __builtin_size_of_val<type>(&Var<0>) -> usize;
+    intrin __builtin_align_of_val<type>(&Var<0>) -> usize;
 }
