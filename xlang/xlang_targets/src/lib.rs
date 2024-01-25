50fe875e0cc001e1508fe0b02d459fa549f5596a
Connor Horman <chorman64@gmail.com>
2024-01-25 10:27:52 -0500

    fix(xlang-targets): Remove broken link to target_tuples::Target in doc

--- xlang/xlang_targets/src/lib.rs
+++ xlang/xlang_targets/src/lib.rs
@@ -2,8 +2,6 @@
 //! Library to describe the behaviours of targets in xlang
 //! Most properties of targets are exposed via this interface, and plugins (including frontends and backends), as well as drivers, should query the properties in this library,
 //! rather than maintaining their own list of properties.
-//!
-//! This library also provides an ABI safe version of the [`target_tuples::Target`] type.
 
 #![deny(warnings, missing_docs, clippy::all, clippy::pedantic, clippy::nursery)]
 #![allow(clippy::module_name_repetitions)]
